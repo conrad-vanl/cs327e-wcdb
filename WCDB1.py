0810897de9b25f26b76d6a7c84f0bda576bff7cd
jupabago <jpbarraza@utexas.edu>
2013-03-07 18:00:59 -0600

    log and documentation

--- WCDB1.py
+++ WCDB1.py
@@ -9,15 +9,18 @@ class WCDB1:
 
     @staticmethod
     def from_file(filename):
+      """This function creates an Element object from an input file"""
       return WCDB1.XML( ET.parse( open(filename) ).getroot() )
 
     @staticmethod
     def from_string(string):
+      """This function creates an Elemenct object from an input string"""
       return WCDB1.XML( ET.fromstring(string) )
 
     def __str__(self):
       return ET.tostring( self.tree )
 
     def export(self, filename):
+      """This function converts an Element object into an xml file"""
       f = open(filename, 'w')
       f.write(self.__str__())
