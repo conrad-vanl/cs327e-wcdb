a61aff00732b2b49ecb867c2a74c09f5f4e5f1d8
Conrad VanLandingham <conrad@landdolph.in>
2013-04-02 10:41:42 -0500

    started WCDB2 mysql stuff

--- WCDB2.py
+++ WCDB2.py
@@ -1,7 +1,22 @@
 import xml.etree.ElementTree as ET
 import os.path
 
+# "Global" vars
+MYSQL_CONNECT = 
+{ 
+  host   : "external-db.s6813.gridserver.com", #"z",
+  user   : "db6813_cs327e", #"<username>",
+  passwd : "grouppassword", #"<password>",
+  db     : "db6813_cs327e" #"downing_test"
+}
+
+# WCDB Wrapper class
 class WCDB:
+
+  """General I/O Factory. This is where all the magic happens"""
+  class Factory:
+    def __init__(self):
+      # nothing for now
 
   """XML i/o utility"""
   class XML:
@@ -33,3 +48,7 @@ class WCDB:
       """Converts an WCDB:XML object into an xml file"""
       f = open(filename, 'w')
       f.write(self.__str__())
+
+
+  """MySQL i/o utility and tools"""
+  class MySQL:
