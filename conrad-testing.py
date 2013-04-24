982e9fdb0f1b2ff19c9b5abab6ea47591cb6928b
Conrad VanLandingham <conrad_vanl@convandesign.com>
2013-04-24 18:59:45 -0500

    fix errors and rename files

--- conrad-testing.py
+++ conrad-testing.py
@@ -3,8 +3,8 @@ import WCDB3
 WCDB3.MYSQL_DEBUG = True
 
 f = WCDB3.Factory()
-f.import_xml( WCDB3.XML.from_file("instance.xml") )
+f.import_xml( WCDB3.XML.from_file("WCDB3.xml") )
 
 xml = f.export_xml()
 
-xml.export("test2.xml")
+xml.export("WCDB3.out.xml")
